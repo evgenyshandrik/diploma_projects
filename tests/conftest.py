095e1858e59c29d06bb583a05d283e5a68b1d8a8
Evgeny Shandrik <shandryk.e@gmail.com>
2022-10-19 00:13:07 +0300

    add request param

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,32 @@
 import pytest
+from dotenv import load_dotenv
+
+DEFAULT_REMOTE_DRIVER = 'selenoid.autotests.cloud'
+
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    """
+    Load .env
+    """
+    load_dotenv()
+
+
+def pytest_addoption(parser):
+    """
+    Parser option
+    """
+    parser.addoption(
+        '--remote_driver',
+        default='selenoid.autotests.cloud'
+    )
 
 
 @pytest.fixture(scope='function', autouse=True)
-def config():
+def config(request):
     """
     Config test
     """
+    remote_driver = request.config.getoption('--remote_driver')
+    remote_driver = remote_driver if remote_driver != "" else DEFAULT_REMOTE_DRIVER
     print("Config")
