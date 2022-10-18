dfcda4b084577423934470db9f3237d5505c6f55
Evgeny Shandrik <shandryk.e@gmail.com>
2022-10-19 00:39:12 +0300

    add config parametra

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,8 @@
 import pytest
 from dotenv import load_dotenv
 
-DEFAULT_REMOTE_DRIVER = 'selenoid.autotests.cloud'
+DEFAULT_WEB_REMOTE_DRIVER = 'selenoid.autotests.cloud'
+DEFAULT_WEB_BROWSER_VERSION = '100'
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -17,8 +18,27 @@ def pytest_addoption(parser):
     Parser option
     """
     parser.addoption(
-        '--remote_driver',
-        default='selenoid.autotests.cloud'
+        '--web_remote_driver',
+        default='selenoid.autotests.cloud',
+        help='web: remote driver (local or selenoid)'
+    )
+
+    parser.addoption(
+        '--web_browser',
+        default='chrome',
+        help='web: browser (chrome or firefox)'
+    )
+
+    parser.addoption(
+        '--web_browser_size',
+        default='1440x800',
+        help='web: browser size (1440x800, 1920x1080, 1280x720)'
+    )
+
+    parser.addoption(
+        '--web_browser_version',
+        default='100',
+        help='web: browser version (100, 90 and etc.)'
     )
 
 
@@ -27,6 +47,8 @@ def config(request):
     """
     Config test
     """
-    remote_driver = request.config.getoption('--remote_driver')
-    remote_driver = remote_driver if remote_driver != "" else DEFAULT_REMOTE_DRIVER
+    web_remote_driver = request.config.getoption('--web_remote_driver')
+    web_remote_driver = web_remote_driver if web_remote_driver != "" else DEFAULT_WEB_REMOTE_DRIVER
+    print(web_remote_driver)
+    print(request.config.getoption('--web_browser'))
     print("Config")
